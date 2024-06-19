2743d7bb50b8c0ff50547c6185aa0c8e2c10f807
virendrapidiha <rohittripathi68316@gmail.com>
2024-06-19 19:19:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,186 @@
+//alert("this is message")  message dene ke liye use hota hai 
+
+
+
+//  var a =prompt("enter your name");
+// console.log("a");
+
+
+// var a=prompt("enter your age")
+// var realAge=Number(age)
+// console.log(age+10) 
+
+//   for(var sam=0;sam<101;sam+=3){
+    // console.log(sam);
+
+//   }
+
+
+
+
+// for(var i=123;i<536;i+=7){
+    // console.log(i);
+
+// }
+
+
+
+// for (let i = 0; i<39; i++){
+    // console.log(i);
+// }
+
+// for(var i=1;i<11;i++){
+    // console.log(i)
+// }
+
+
+// var j=11;
+// while(j<21){
+    // console.log(j)
+    // j++;
+// }
+
+
+
+
+// var age=prompt("enter your age");
+// console.log(parentInt(age+2));
+
+
+
+
+// var n1=Number(prompt("number1"))
+// var n2=Number(prompt("number2"))
+// console.log(n1+n2);
+
+
+
+
+
+
+// var arr=[1,2,34,5.6]
+// for(var i=0;i<5;i++){
+    // console.log(arr[i])
+// }
+
+
+
+
+
+//  arrayvar=[1,2,34,5]
+// array.forEach(function(val){
+    // console.log(val)
+
+// })
+
+
+
+
+// var arr=[1,2,3,4]
+// arr.push();
+
+
+
+
+// var abc=20;
+// console.log(abc);
+
+
+//  var ans=prompt("enter your age")
+
+
+// var a=30;
+// var b=10;
+// console.log(a+b);
+
+
+
+
+// var a="sarthak";
+// var b="sharma";
+// console.log(a-b);
+
+
+
+// var num1=+prompt("enter the age")
+// var num2=+prompt("enter the year")
+// console.log(num1+num2);
+
+
+
+
+
+// ask a user his marks in english ,hindi and math and print average
+// var a=Number(prompt("enter your number in engilsh"));
+// var b=Number(prompt("enter your number in hindi"));
+// var c=Number(prompt("enter your number in math"));
+// var avg=(a+b+c)/3;
+// console.log(avg);
+
+
+
+// ask a useer his age and if 18+ you can vote and if not  tell him after him after how many years he can vote 
+// var age = (prompt("Please enter your age:"))
+// if (age > 18) {
+//     console.log("You are eligible to vote!");
+// } else {
+//     console.log("you are not eligible");
+// }
+
+
+// 1) Make a variable user and initialize a value "Ram" and print it on console
+// var a=ram;
+// console.log(a);
+
+
+// 2) Make two variables and print their sum, mul, div, sub
+// var a=10;
+// var b=5;
+// console.log(a+b);
+
+//  var a=10;
+//  var b=5;
+//  console.log(a-b);   
+
+// var a=10;
+// var b=5;
+// console.log(a*b);
+
+// var a=10;
+// var b=5;
+// console.log(a/b);
+
+
+// 3) Write a JavaScript program to find the area of a triangle where three sides are 5, 6,7
+// var side1 = 5;
+// var side2 = 6;
+// var side3 = 7;
+
+// // Calculate the area of the triangle
+// var area = calculateTriangleArea(side1, side2, side3);
+// console.log("The area of the triangle is: " + area);
+
+// 4.)write a javascript program compute the sum of the two given integers. If the two values are the same, then print triple their sum.
+// var num1 = 5;
+// var num2 = 5;
+
+// // Compute the sum or triple the sum 
+// var result = computeSumOrTriple(num1, num2);
+// console.log("The result is: " + result);
+
+
+// // 5) Accept a number from a user and print it on console
+// var a = prompt("Please enter a number:");
+// var number = (a);
+// console.log("The number you entered is: " + number);
+
+
+// 6)Ask a user his/her name and say hello (eg. "Hello keshav")
+
+var a=(prompt("enter your name"));
+var b="hello keshav"
+console.log("b");
+
+
+
+
---
+++ script2.js
@@ -0,0 +1,16 @@
+var a=[1 ,2,3,4,5,6,7,8]
+ 
+
+
+a.forEach(function(keshav){
+    console.log(keshav)
+});
+
+
+
+
+const array = [1, 2, 3, 4, 5];
+
+array.forEach(function(element) {
+  console.log(element);
+});
